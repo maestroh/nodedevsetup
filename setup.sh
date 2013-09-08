4e47c6175e5fd25afdb22a3607d2fe5626ee3b7a
maestroh <nerdupgeekdown@gmail.com>
2013-09-07 21:27:30 -0500

    added mongo and node to setup

--- setup.sh
+++ setup.sh
@@ -1,12 +1,32 @@
+sudo apt-get install g++ curl libssl-dev apache2-utils
 sudo apt-get install -y git-core
+sudo apt-get remove make
+sudo apt-get install make
 
 cd $HOME
-if [ -d ./dotfile/ ]; then
-  mv dotfile dotfile.old
-fi
 
+# setup node#
+sudo apt-get update
+sudo apt-get install python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
+
+# setup mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install mongodb-10gen
+
+
+# setup vundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +BundleInstall +qall
+
+# setup dotfiles
+if [ -d ./dotfile/ ]; then
+  rm -rf dotfile
+fi
 
 git clone https://github.com/maestroh/dotfile.git
 ln -sb dotfile/.screenrc .
