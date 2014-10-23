00adba29b4180de9cec9ffe9ae1f4cc0d7f1a1c8
nael <nerdupgeekdown@gmail.com>
2014-10-23 16:28:53 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,20 +17,9 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update
 sudo apt-get install mongodb-10gen
+sudo echo 'smallfiles = true' >> /etc/mongod.conf
+sudo service mongod start
 
-
-# setup vundle
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim +BundleInstall +qall
-
-# setup dotfiles
-if [ -d ./dotfile/ ]; then
-  rm -rf dotfile
-fi
-
-git clone https://github.com/maestroh/dotfile.git
-ln -sb dotfile/.screenrc .
-ln -sb dotfile/.vimrc .
 
 
 
